557fc3959b1a8a419ecbcf1c9cc5d43fef84dfbd
raj123raj <tltpriya2003@gmail.com>
2021-06-10 15:04:24 +0530

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import detailsByZipCode from './views/detailsByZipCode.vue'
-//import CustomerDetails from './views/CustomerDetails.vue'
-//Refer https://github.com/aditya-sridhar/simple-vuejs-app
 Vue.use(Router)
 
 const router =  new Router({
