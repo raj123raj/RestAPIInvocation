41966e88da6a12be3b87dc067b80082f57d87dae
raj123raj <tltpriya2003@gmail.com>
2021-06-11 10:28:51 +0530

    Update router.js
    Given additional router for getting exchange rates

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import detailsByZipCode from './views/detailsByZipCode.vue'
+import getExchangeRates from './views/getExchangeRates.vue'
 Vue.use(Router)
 
 const router =  new Router({
@@ -13,6 +14,11 @@ const router =  new Router({
       path: '/detailsByZipCode',
       name: 'detailsByZipCode',
       component: detailsByZipCode
+    },
+    {
+      path: '/getExchangeRates',
+      name: 'getExchangeRates',
+      component: getExchangeRates
     }
   ]
 })
